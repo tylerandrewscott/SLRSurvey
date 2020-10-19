1e19027c13eb4886a3472c1247358c1adff07408
Kyra Skye <kgmoserd@ucdavis.edu>
2020-10-19 14:28:06 -0700

    Creating Initial R Script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SLRSurvey
 Repository for R Data of Bay Area Sea Level Rise Governance Survey (Fall 2020)
+
+
---
+++ SLRSurvey_Script_10-19-20_V1.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/kyras/OneDrive/Desktop/SLRSurvey/SLRSurvey")
+install.packages(c("igraph", "netrankr"))
+library(igraph)
+library(netrankr)
