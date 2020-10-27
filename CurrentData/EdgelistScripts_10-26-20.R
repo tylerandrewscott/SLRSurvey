a6ba05f7d5ee6693dc505da02497113bbfdb4d7e
Kyra Skye <kgmoserd@ucdavis.edu>
2020-10-27 11:37:11 -0700

    Created policies and concerns single networks

--- CurrentData/EdgelistScripts_10-26-20.R
+++ CurrentData/EdgelistScripts_10-26-20.R
@@ -2,7 +2,9 @@ setwd("C:/Users/kyras/OneDrive/Desktop/SLRSurvey/SLRSurvey/CurrentData")
 library(igraph)
 library(netrankr)
 
+#Concerns to concerns edgelist----------------------------------------------------
 Concerns_Raw <- read.csv("SLR_raw_concerns.csv")
+Concerns_Raw[555,3]="Ecosystem" #one typo in dataset
 
 #create edgelist that is only 2 columns for concerns
 x<-Concerns_Raw #names my 3 column spreadsheet as object x
@@ -14,6 +16,8 @@ concernedgelist <- concernedgelist[rowSums(is.na(concernedgelist))==0, ]
 
 #remove any pairs that are missing one or both concerns
 finconcernedge<-concernedgelist[!(concernedgelist$C1==""|concernedgelist$C2==""), ]
+
+#policies to policies edgelist--------------------------------------------------------
 
 #create edgelist that is only 2 columns for policy priorities
 Policies_Raw <- read.csv("SLR_raw_policies.csv")
@@ -27,6 +31,8 @@ policyedgelist <- policyedgelist[rowSums(is.na(policyedgelist))==0, ]
 #remove any pairs that are missing one or both policies
 finpolicyedge<-policyedgelist[!(policyedgelist$P1==""|policyedgelist$P2==""), ]
 
+#barriers to barriers edgelist-----------------------------------------------------
+
 #create edgelist that is only 2 columns for barriers
 Barriers_Raw <- read.csv("SLR_raw_barriers.csv")
 z <-Barriers_Raw #names my 3 column spreadsheet as object z
@@ -39,5 +45,120 @@ barrieredgelist <- barrieredgelist[rowSums(is.na(barrieredgelist))==0, ]
 #remove any pairs that are missing one or both barriers
 finbarrieredge<-barrieredgelist[!(barrieredgelist$B1==""|barrieredgelist$B2==""), ]
 
-#start trying to do the network for policies
+#policies to policies network-----------------------------------------------
+#start trying to do the network for concerns
+set.seed(2)
+policiesnet=graph_from_data_frame(finpolicyedge, directed=FALSE) 
+policiesnet
+
+#try to visualize the policies
+plot(policiesnet)
+plot(policiesnet, layout=layout_with_fr(policiesnet))
+dev.off()
+
+#start looking into measuring policies network
+degree(policiesnet) #degree centrality
+betweenness(policiesnet) #betweenness centrality
+closeness(policiesnet) #number steps required to access every node from a given node
+
+#create dataset of node-level measures
+names=V(policiesnet)$name
+de=degree(policiesnet)
+st=graph.strength(policiesnet)
+be=betweenness(policiesnet, normalized=T)
+nodedata_policies=data.frame(node.name=names, degree=de, strength=st, betweenness=be)
+head(nodedata_policies)
+
+#realized since unweighted network strength and degree will be same I think
+plot(strength~degree, data=nodedata_policies)
+
+#relationship between strength and betweenness in policies
+plot(betweenness~strength, data=nodedata_policies)
+
+#calculate number of nodes (16) and edges (1937) for policies
+nodenum_policies=vcount(policiesnet)
+edgesnum_policies=ecount(policiesnet)
+
+nodenum_policies
+edgesnum_policies
+
+#calculate the density
+dyads_policies=(nodenum_policies*(nodenum_policies -1))/2
+density_policies=edgesnum_policies/dyads_policies
+density_policies
+#can also just use function
+edge_density(policiesnet)
+
+#plot the degree distribution (statistical distribution of node degrees in network) for policies
+hist(degree(policiesnet), breaks=10, col="gray")
+
+#plot probability densities of each degree (proportion of nodes with degree 1, 2, etc.) for policies network
+plot(degree.distribution(policiesnet), pch=19)
+
+#paths for policies network- results are 1 or 2 for most since all policies tend to be picked once by at least one person
+paths_policies=distances(policiesnet, algorithm="unweighted")
+paths_policies
+
+#community detection
+eb_policies=edge.betweenness.community(policiesnet)
+eb_policies
+length(eb_policies) #number of communiteis
+modularity(eb_policies) #modularity
+membership(eb_policies) #assignment of nodes to communities
+plot(eb_policies, policiesnet)
+
+#concerns network-------------------------------------------------------------------------
+
+
+set.seed(2)
+concernsnet=graph_from_data_frame(finconcernedge, directed=FALSE) 
+concernsnet
+
+#try to visualize the concerns
+plot(concernsnet)
+plot(concernsnet, layout=layout_with_fr(concernsnet))
+dev.off()
+
+#start looking into measuring concerns network
+degree(concernsnet) #degree centrality
+betweenness(concernsnet) #betweenness centrality
+closeness(concernsnet) #number steps required to access every node from a given node
+
+#create dataset of node-level measures
+namesconcern=V(concernsnet)$name
+deconcern=degree(concernsnet)
+stconcern=graph.strength(concernsnet)
+beconcern=betweenness(concernsnet, normalized=T)
+nodedata_concerns=data.frame(node.name=namesconcern, degree=deconcern, strength=stconcern, betweenness=beconcern)
+head(nodedata_concerns)
+
+#realized since unweighted network strength and degree will be same I think
+plot(strength~degree, data=nodedata_concerns)
+
+#relationship between strength and betweenness in concerns
+plot(betweenness~strength, data=nodedata_concerns)
+
+#density
+edge_density(concernsnet)
+
+#plot the degree distribution (statistical distribution of node degrees in network) for concerns
+hist(degree(concernsnet), breaks=10, col="gray")
+
+#plot probability densities of each degree (proportion of nodes with degree 1, 2, etc.) for concerns network
+plot(degree.distribution(concernsnet), pch=19)
+
+#paths for concerns
+paths_concerns=distances(concernsnet, algorithm="unweighted")
+paths_concerns
+
+#community detection
+eb_concerns=edge.betweenness.community(concernsnet)
+eb_concerns
+length(eb_concerns) #number of communities
+modularity(eb_concerns) #modularity
+membership(eb_concerns) #assignment of nodes to communities
+plot(eb_concerns, concernsnet)
+
+#Barriers network-------------------------------------------------------------------------
+
 
