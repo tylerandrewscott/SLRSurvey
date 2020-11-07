b1cc3be7e64d9b50e69521394fde4c22c906d6c5
Kyra Skye <kgmoserd@ucdavis.edu>
2020-11-06 17:01:04 -0800

    Progress on all combined single layer network

--- CurrentData/CombinedSingleLayerNetwork.R
+++ CurrentData/CombinedSingleLayerNetwork.R
@@ -1,6 +1,7 @@
 setwd("C:/Users/kyras/OneDrive/Desktop/SLRSurvey/SLRSurvey/CurrentData")
 library(igraph)
 library(netrankr)
+library(data.table)
 
 #Create All Pairs Edgelist--------------------------
 #Create edgelist with all policies, concerns, barrier pairs in 2 columns
@@ -15,11 +16,36 @@ comboedgelist <- comboedgelist[rowSums(is.na(comboedgelist))==0, ]
 #remove any pairs that are missing one or both in a pair for final edge list
 fincomboedge<-comboedgelist[!(comboedgelist$I1==""|comboedgelist$I2==""), ]
 
-#Create weighted edgelist
-library(data.table)
+#Create weighted edgelist------------------------------------------------------------
 #create data table for policies from existing edge list
 combined.data.table <- data.table(fincomboedge)
 #create new spreadsheet with number of occurences for each node pair
 weightedcomboedge <- combined.data.table[ , .N, by=.(combined.data.table$I1, combined.data.table$I2)]
 
-#Create Network
+#rename data table columns for use as weighted edgelist
+setnames(weightedcomboedge, "N", "weight")
+setnames(weightedcomboedge, "combined.data.table", "I1")
+setnames(weightedcomboedge, "combined.data.table.1", "I2")
+
+#Create Network--------------------------------------------------------------------
+#Load in vertex attribute
+attrib <- read.csv("SLR_Combined_VectorTypes.csv")
+
+#Match vector attributes
+
+weightedcomboedge$TypeI1=factor(attrib[match(weightedcomboedge$I1, attrib$Vector), "Type"])
+
+weightedcomboedge$TypeI2=factor(attrib[match(weightedcomboedge$I2, attrib$Vector), "Type"])
+
+weightedcomboedge <- weightedcomboedge[ , c("I1", "I2", "weight", "TypeI1", "TypeI2")]
+
+weightedcomboedge$colorI1=c("darkseagreen", "goldenrod", "slateblue")[as.numeric(weightedcomboedge$TypeI1)]
+
+weightedcomboedge$colorI2=c("darkseagreen", "goldenrod", "slateblue")[as.numeric(weightedcomboedge$TypeI2)]
+
+#create network
+set.seed(2)
+combinednet=graph_from_data_frame(weightedcomboedge, directed=FALSE)
+combode=igraph::degree(combinednet)
+
+plot(combinednet, layout=layout_with_graphopt, vertex.size=combode, edge.width=weightedcomboedge$weight, edge.color="black", )
