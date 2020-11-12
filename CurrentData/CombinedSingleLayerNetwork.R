f41440198d05a60fa6eb41da78aa99a97ace35b5
Kyra Skye <kgmoserd@ucdavis.edu>
2020-11-12 09:53:28 -0800

    Create edge colors and create second network for only edge weights >10 and no 0 degree vertices

--- CurrentData/CombinedSingleLayerNetwork.R
+++ CurrentData/CombinedSingleLayerNetwork.R
@@ -30,7 +30,8 @@ setnames(weightedcomboedge, "combined.data.table.1", "I2")
 #Create Network--------------------------------------------------------------------
 #Load in vertex attribute
 attrib <- read.csv("SLR_Combined_VectorTypes.csv")
-#try to create edge attributes in weighted edgelist
+
+#try to create edge attribute for type of edge inside weighted edgelist
 weightedcomboedge$Type1 <- attrib$Type[match(weightedcomboedge$I1, attrib$Vector)]
 
 weightedcomboedge$Type2 <- attrib$Type[match(weightedcomboedge$I2, attrib$Vector)]
@@ -38,7 +39,7 @@ weightedcomboedge$Type2 <- attrib$Type[match(weightedcomboedge$I2, attrib$Vector
 weightedcomboedge$edgetype <- "NA"
 
   weightedcomboedge[weightedcomboedge$Type1=="Policy" & weightedcomboedge$Type2=="Policy"]$edgetype <- "Policy-Policy"
-  weightecomboedge[weightedcomboedge$Type1=="Concern" & weightedcomboedge$Type2=="Concern"]$edgetype <- "Concern-Concern"
+  weightedcomboedge[weightedcomboedge$Type1=="Concern" & weightedcomboedge$Type2=="Concern"]$edgetype <- "Concern-Concern"
   weightedcomboedge[weightedcomboedge$Type1=="Barrier" & weightedcomboedge$Type2=="Barrier"]$edgetype <- "Barrier-Barrier"
   weightedcomboedge[weightedcomboedge$Type1=="Policy"& weightedcomboedge$Type2=="Concern"]$edgetype <- "Policy-Concern"
   weightedcomboedge[weightedcomboedge$Type1=="Policy"& weightedcomboedge$Type2=="Barrier"]$edgetype <- "Policy-Barrier"
@@ -47,9 +48,15 @@ weightedcomboedge$edgetype <- "NA"
   weightedcomboedge[weightedcomboedge$Type1=="Concern"& weightedcomboedge$Type2=="Barrier"]$edgetype <- "Barrier-Concern"
   weightedcomboedge[weightedcomboedge$Type1=="Concern"& weightedcomboedge$Type2=="Policy"]$edgetype <- "Policy-Concern"
 
-####START BACK HERE WITH CREATING EDGE COLOR-----------------------------------------
+  #create color edge type attribute in weighted edge list
+weightedcomboedge$edgecolor <- "NA"  
+weightedcomboedge[weightedcomboedge$edgetype=="Policy-Policy"]$edgecolor <- "#dcdcdc"
+weightedcomboedge[weightedcomboedge$edgetype=="Barrier-Barrier"]$edgecolor <- "#dcdcdc"
+weightedcomboedge[weightedcomboedge$edgetype=="Concern-Concern"]$edgecolor <- "#dcdcdc"
+weightedcomboedge[weightedcomboedge$edgetype=="Policy-Concern"]$edgecolor <- "#808080"
+weightedcomboedge[weightedcomboedge$edgetype=="Policy-Barrier"]$edgecolor <- "#808080"
+weightedcomboedge[weightedcomboedge$edgetype=="Barrier-Concern"]$edgecolor <- "#808080"
   
-
 #create network
 set.seed(2)
 combinednet=graph_from_data_frame(weightedcomboedge, directed=FALSE)
@@ -63,10 +70,19 @@ combinednet <- set.vertex.attribute(combinednet, "Color", index=attrib$Vector, v
 list.vertex.attributes(combinednet)
 get.vertex.attribute(combinednet)
 
-#plot with vertex color using type attribute
-plot(combinednet, layout=layout_with_graphopt, vertex.size=combode/4, edge.width=weightedcomboedge$weight/10, vertex.color=V(combinednet)$Color, vertex.label.cex=0.5)
+list.edge.attributes(combinednet)
 
 
+#plot with vertex color using attributes
+plot(combinednet, layout=layout_with_graphopt, vertex.size=combode/4, edge.width=weightedcomboedge$weight/10, vertex.color=V(combinednet)$Color, vertex.label.cex=0.5, edge.color=weightedcomboedge$edgecolor)
 
+plot(combinednet, layout=layout_with_fr, vertex.size=combode/4, edge.width=weightedcomboedge$weight/10, vertex.color=V(combinednet)$Color, vertex.label.cex=0.5, edge.color=weightedcomboedge$edgecolor)
 
+#try and remove edges below certain weight
+
+tenedgethreshold <- combinednet
+tenedgethreshold<- delete.edges(tenedgethreshold, which(E(tenedgethreshold)$weight <10)-1)
+tende=igraph::degree(tenedgethreshold)
+tenedgethreshold <- delete.vertices(tenedgethreshold, V(tenedgethreshold)[degree(tenedgethreshold)==0])
+plot(tenedgethreshold, layout=layout_with_graphopt, vertex.size=tende/4, edge.width=E(tenedgethreshold)$weight/15, vertex.color=V(tenedgethreshold)$Color, vertex.label.cex=0.5, edge.color=E(tenedgethreshold)$edgecolor)
 
