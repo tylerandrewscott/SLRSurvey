de4548b011c7147da3bffa5a5b7ecbc2ac560b8e
Kyra Skye <kgmoserd@ucdavis.edu>
2020-11-24 13:02:27 -0800

    OLS Regression on level of involvement

--- Current Files/Code/InvolvementRegressions.R
+++ Current Files/Code/InvolvementRegressions.R
@@ -11,4 +11,10 @@ unique(survey$Q1_Num)
 
 #lm(formula= survey_LOIcomplete$LOI_Numeric~VARIABLES, data=survey_LOIcomplete)
 
-lm(formula=survey$Q1_Num~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F, data=survey)
+ols_LOI <- lm(formula=survey$Q1_Num~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F+survey$Q32_Sum+survey$Q11_LTAware+survey$Q11_STAware+survey$Q12_LTConcern+survey$Q12_STConcern+survey$WhenSLR+survey$ActionAgree+survey$RiskAgree+survey$Q20_Sum, data=survey)
+
+sumols_LOI <- summary(ols_LOI)
+
+sumols_LOI
+
+#
