3ce82d20ce3cfc368f6e3cfadb69adc67d809f87
Kyra Skye <kgmoserd@ucdavis.edu>
2020-11-24 09:06:26 -0800

    Start working on personal involvement regressions

--- Current Files/Code/InvolvementRegressions.R
+++ Current Files/Code/InvolvementRegressions.R
@@ -1,16 +1,14 @@
+setwd("C:/Users/kyras/OneDrive/Desktop/SLRSurvey/SLRSurvey/Current Files/Data")
 library(tidyverse)
 library(MASS)
 
 #load data
-survey <- read_csv("Data/SLRSurvey_Full.csv")
-
-#Remove NAs from level of involvement ordinal data---------------------------------------
-summary(survey$Q1_Num)
-survey$LOI_Numeric <- as.numeric(survey$Q1_Num)
-
-survey_LOIcomplete <- survey %>% 
-  filter(!is.na(survey$LOI_Numeric))
+survey <- read_csv("Survey_Regressions.csv")
 
 #OLS regression on level of involvement------------------
 
+unique(survey$Q1_Num)
+
 #lm(formula= survey_LOIcomplete$LOI_Numeric~VARIABLES, data=survey_LOIcomplete)
+
+lm(formula=survey$Q1_Num~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F, data=survey)
