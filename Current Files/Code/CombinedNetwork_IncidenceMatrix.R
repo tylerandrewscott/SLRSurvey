e5fbfca68e860c615cd64f1432338395176b6e5a
Kyra Skye <kgmoserd@ucdavis.edu>
2020-11-25 10:56:45 -0800

    Fix adjacency matrix column names etc.

---
+++ Current Files/Code/CombinedNetwork_IncidenceMatrix.R
@@ -0,0 +1,10 @@
+setwd("C:/Users/kyras/OneDrive/Desktop/SLRSurvey/SLRSurvey/Current Files/Data/Combined Network")
+library(igraph)
+library(netrankr)
+library(data.table)
+
+#edges = data.table(numbers = 1:26,letters = letters)
+#as.matrix(table(edges$numbers,edges$numbers))
+#as.data.table(table(edges$numbers,edges$numbers))
+
+
--- Current Files/Code/InvolvementRegressions.R
+++ Current Files/Code/InvolvementRegressions.R
@@ -1,6 +1,12 @@
 setwd("C:/Users/kyras/OneDrive/Desktop/SLRSurvey/SLRSurvey/Current Files/Data")
 library(tidyverse)
 library(MASS)
+#install.packages("AER")
+library(AER)
+#install.packages("pscl")
+library(pscl)
+#install.packages("VGAM")
+library(VGAM)
 
 #load data
 survey <- read_csv("Survey_Regressions.csv")
@@ -11,6 +17,11 @@ unique(survey$Q1_Num)
 
 #lm(formula= survey_LOIcomplete$LOI_Numeric~VARIABLES, data=survey_LOIcomplete)
 
+
+survey$Q1_Num <- as.numeric(survey$Q1_Num)
+unique(survey$Q1_Num)
+#NEED TO FIX FACTORS ASSIGNMENT TO Q1 BEFORE DOING THESE REGRESSIONS
+
 ols_LOI <- lm(formula=survey$Q1_Num~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F+survey$Q32_Sum+survey$Q11_LTAware+survey$Q11_STAware+survey$Q12_LTConcern+survey$Q12_STConcern+survey$WhenSLR+survey$ActionAgree+survey$RiskAgree+survey$Q20_Sum, data=survey)
 
 sumols_LOI <- summary(ols_LOI)
@@ -18,8 +29,14 @@ sumols_LOI <- summary(ols_LOI)
 sumols_LOI
 
 #Ordinal Logistic Regression -------------------
-survey$Q1_fact <- as.factor(survey$Q1_Num)
+
 polr_LOI <- polr(formula=survey$Q1_fact~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F+survey$Q32_Sum+survey$Q11_LTAware+survey$Q11_STAware+survey$Q12_LTConcern+survey$Q12_STConcern+survey$WhenSLR+survey$ActionAgree+survey$RiskAgree+survey$Q20_Sum, Hess=TRUE, data=survey)
 sumpolr_LOI <- summary(polr_LOI)
 sumpolr_LOI
+coeftest(polr_LOI)
+pR2(polr_LOI)
+
+#Generalized Ordinal Logistic Regression --------------
+#relaxes the proportional odds assumption 
+gologit_LOI <- vglm(formula=survey$LOI_factor~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F+survey$Q32_Sum+survey$Q11_LTAware+survey$Q11_STAware+survey$Q12_LTConcern+survey$Q12_STConcern+survey$WhenSLR+survey$ActionAgree+survey$RiskAgree+survey$Q20_Sum, Hess=TRUE, data=survey, parallel=FALSE, family=cumulative)
 
