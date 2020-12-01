4b2a898dac9bc58a7075a5d9a7e9c563993c6e58
Kyra Skye <kgmoserd@ucdavis.edu>
2020-12-01 10:28:51 -0800

    Work on regressions- trying to fix class of variables to get vglm () to work (still not working)

--- CurrentFiles/Code/InvolvementRegressions.R
+++ CurrentFiles/Code/InvolvementRegressions.R
@@ -7,6 +7,7 @@ library(AER)
 library(pscl)
 #install.packages("VGAM")
 library(VGAM)
+library(dplyr)
 
 #load data
 survey <- read_csv("Survey_Regressions.csv")
@@ -22,6 +23,9 @@ sumols_LOI <- summary(ols_LOI)
 sumols_LOI
 
 #Ordinal Logistic Regression -------------------
+survey$Q1_numcheck <- survey$Q1_Num
+survey$Q1_fact <- as.factor(survey$Q1_Num2)
+levels(survey$Q1_fact)
 
 polr_LOI <- polr(formula=survey$Q1_fact~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F+survey$Q32_Sum+survey$Q11_LTAware+survey$Q11_STAware+survey$Q12_LTConcern+survey$Q12_STConcern+survey$WhenSLR+survey$ActionAgree+survey$RiskAgree+survey$Q20_Sum, Hess=TRUE, data=survey)
 sumpolr_LOI <- summary(polr_LOI)
@@ -31,5 +35,22 @@ pR2(polr_LOI)
 
 #Generalized Ordinal Logistic Regression --------------
 #relaxes the proportional odds assumption 
-gologit_LOI <- vglm(formula=survey$LOI_factor~survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F+survey$Q32_Sum+survey$Q11_LTAware+survey$Q11_STAware+survey$Q12_LTConcern+survey$Q12_STConcern+survey$WhenSLR+survey$ActionAgree+survey$RiskAgree+survey$Q20_Sum, Hess=TRUE, data=survey, parallel=FALSE, family=cumulative)
+survey = filter(survey, WhenSLR != "#NULL!")
+survey = filter(survey, RiskAgree != "#NULL!")
+survey = filter(survey, ActionAgree != "#NULL!")
+survey = filter(survey, Q11_STAware != "#NULL!")
+survey = filter(survey, Q11_LTAware != "#NULL!")
+survey = filter(survey, Q12_STConcern != "#NULL!")
+survey = filter(survey, Q12_LTConcern != "#NULL!")
+
+survey$WhenSLR <- as.numeric(survey$WhenSLR)
+survey$ActionAgree <- as.numeric(survey$ActionAgree)
+survey$RiskAgree <- as.numeric(survey$RiskAgree)
+survey$Q11_STAware <- as.numeric(survey$Q11_STAware)
+survey$Q11_LTAware <- as.numeric(survey$Q11_LTAware)
+survey$Q12_STConcern <- as.numeric(survey$Q12_STConcern)
+survey$Q12_LTConcern <- as.numeric(survey$Q12_LTConcern)
+
+
+gologit_LOI <- vglm(formula=survey$Q1_Num ~ survey$Q2_Personal+survey$Q2_MultiOrg+survey$Q5_F+survey$Q32_Sum+survey$Q11_LTAware+survey$Q11_STAware+survey$Q12_LTConcern+survey$Q12_STConcern+survey$WhenSLR+survey$ActionAgree+survey$RiskAgree+survey$Q20_Sum, data=survey, Hess=TRUE, parallel=FALSE, family=cumulative)
 
