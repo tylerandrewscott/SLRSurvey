e23e48316442ae15345c8a5bac2e13670f2c10b0
Kyra Skye <kgmoserd@ucdavis.edu>
2021-02-19 09:56:28 -0800

    Finished merge of survey data with respondent group probabilities- still need to examine/analyze for findings

--- CurrentFiles/Code/ltagroupanalysis.R
+++ CurrentFiles/Code/ltagroupanalysis.R
@@ -1,5 +1,5 @@
 
-#put in all the setup code from Tyler's R markdown-------------------
+#Put in all the setup code from Tyler's R markdown-------------------
 
 packs =c('tidyverse','purrr','data.table','statnet','latentnet','bipartite','lvm4net',
          'ggthemes','here','ggnetwork','gridExtra','ggrepel','corrplot')
@@ -8,17 +8,17 @@ library(purrr)
 library(data.table)
 library(statnet)
 library(latentnet)
-install.packages("bipartite")
+#install.packages("bipartite")
 library(bipartite)
-install.packages("lvm4net")
+#install.packages("lvm4net")
 library(lvm4net)
-install.packages("ggthemes")
+#install.packages("ggthemes")
 library(ggthemes)
-install.packages("here")
-install.packages("ggnetwork")
-install.packages("gridExtra")
-install.packages("ggrepel")
-install.packages("corrplot")
+#install.packages("here")
+#install.packages("ggnetwork")
+#install.packages("gridExtra")
+#install.packages("ggrepel")
+#install.packages("corrplot")
 library(here)
 library(ggnetwork)
 library(gridExtra)
@@ -101,7 +101,7 @@ htmlTable(cbind(as.data.table(soc['Regional Authority',],keep.rownames = T)[orde
                 as.data.table(soc['Collab',],keep.rownames = T)[order(-V2),][1:10,1]))
 
 
-#start of Tyler's lta/lca code------------------------------
+#Start of Tyler's lta/lca code------------------------------
 
 #### note -- mlta takes vectors instead of single D and G values -- but pblapply is used to parallelize #####
 opts = data.table(expand.grid(D = 1,G = 2,fix = c(F,T)))
@@ -143,31 +143,54 @@ ggplot(beta_dt,aes(x = `Group 1`,y = `Group 2`,label = rn)) + geom_abline(lty = 
   geom_point(pch = 21,col = 'grey50') + geom_text_repel(size = 2.5,max.overlaps = 100,min.segment.length = 0.2) +
   xlab('intercept, group 1') + ylab('intercept, group 2') + theme_bw() + ggtitle('Intercept estimates for item-response by group','p = 1 / exp(-beta)')
 
-#create list of group probabilities for respondents------------------------
-#looks like group_probs is the probabilities
-#need to figure out how to match that to the respondent IDs and then the survey
-#row names of Y has the respondent IDs so could match group probs to Y (both 1:698 rows) and then from there match to respondent ID from Y to orig
-#match that back to survey results--------------------------------------
+#Create list of group probabilities for respondents------------------------
+#looks like group_probs is the probabilities and Y is cooccurrence matrix with row names as respondent ID
+
+group_probs
+class(group_probs)
+
+group_prob <- as.data.frame(group_probs)
+#create dataframe of Y (row names are respondent ID) and add in the group 1 and group 2 probabilities
+RespGroups <- as.data.frame(Y)
+RespGroups$P_G1 <- group_prob$V1
+RespGroups$P_G2 <- group_prob$V2
+class(RespGroups)
+
+RespGroups$respondentID <- row.names(RespGroups)
+
+#Match that back to survey results--------------------------------------
+?merge()
+survey_orig <- as.data.frame(orig)
+RespGroups <- as.data.frame(RespGroups)
+survey_orig$respondentID <- survey_orig$ResponseId
+
+merged_survey <- left_join(RespGroups, survey_orig, by='respondentID')
+
+#Use merged_survey to find connections between groups and survey attributes--------------
+
+#Ideas: correlations between probabilities and different properties, assign group by probability and correlate group membership with properties
+#Potential attributes/properties: org type, time in position, SLR concern, info types used, SLR awareness
 
 
-#redo from corplot.Rmd to replace mtla_results.rds with original one again (accidentally replaced it when I ran my code copy to only have D=1 and G=2but now mine is mtla_results2.rds)----------------------
+#Ignore this section -----------------------------------------------------
+#redo from corplot.Rmd to replace mtla_results.rds with original one again (accidentally replaced it when I ran my code copy to only have D=1 and G=2but now mine is mtla_results2.rds)
 
-opts = data.table(expand.grid(D = 0:4,G = 1:5,fix = c(F,T)))
-opts = opts[D>0|!fix,]
-require(doParallel)
-cluster = makeCluster(4)
-registerDoParallel(cluster)
-clusterExport(cl = cluster,varlist = list('opts','Y'))
-clusterEvalQ(cl = cluster,require(lvm4net))
-mlta_tests = foreach(i = 1:nrow(opts)) %dopar% {mlta(X = Y, D = opts$D[i], G = opts$G[i],wfix = opts$fix[i],nstarts = 5,maxiter = 1e3)}
-saveRDS(mlta_tests,'scratch/mlta_results.rds')
+#opts = data.table(expand.grid(D = 0:4,G = 1:5,fix = c(F,T)))
+#opts = opts[D>0|!fix,]
+#require(doParallel)
+#cluster = makeCluster(4)
+#registerDoParallel(cluster)
+#clusterExport(cl = cluster,varlist = list('opts','Y'))
+#clusterEvalQ(cl = cluster,require(lvm4net))
+#mlta_tests = foreach(i = 1:nrow(opts)) %dopar% {mlta(X = Y, D = opts$D[i], G = opts$G[i],wfix = opts$fix[i],nstarts = 5,maxiter = 1e3)}
+#saveRDS(mlta_tests,'scratch/mlta_results.rds')
 
-mlta_tests = readRDS('scratch/mlta_results.rds')
-mlta_results = data.table(opts,BIC = sapply(mlta_tests,function(x) x$BIC))
-mlta_results$G_fix = paste0('G = ',mlta_results$G,' (fixed slope = ',mlta_results$fix,')')
-mlta_results$BIC <- round(mlta_results$BIC)
-mlta_cast = dcast(mlta_results[order(BIC)],G_fix ~ D,value.var = 'BIC')
+#mlta_tests = readRDS('scratch/mlta_results.rds')
+#mlta_results = data.table(opts,BIC = sapply(mlta_tests,function(x) x$BIC))
+#mlta_results$G_fix = paste0('G = ',mlta_results$G,' (fixed slope = ',mlta_results$fix,')')
+#mlta_results$BIC <- round(mlta_results$BIC)
+#mlta_cast = dcast(mlta_results[order(BIC)],G_fix ~ D,value.var = 'BIC')
 
 
-htmlTable(mlta_cast[,-1],caption = 'BIC scores by MLTA specification',rnames = mlta_cast$G_fix,header = paste0('D = ',0:4),
-          tfoot = '*fixed slope refers to whether slope is constant across all groups are group-specific')
+#htmlTable(mlta_cast[,-1],caption = 'BIC scores by MLTA specification',rnames = mlta_cast$G_fix,header = paste0('D = ',0:4),
+ #         tfoot = '*fixed slope refers to whether slope is constant across all groups are group-specific')
